1cbbd349177b14037a763053de4aae0d345fb6ea
Sumit R Malik <malik.r.sumit@gmail.com>
2025-07-20 18:04:31 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,7 +15,7 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Hi GFG Workshop'
+    expected_text = 'Hi GFG Workshop. Updated.'
     assert expected_text.encode() in response.data
 
 
